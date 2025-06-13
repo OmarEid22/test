db6585ccf94848440ac56155fb76c292f18d4878
myyyh03 <myyy003@hotmail.com>
2025-06-13 19:53:34 +0300

    fixed multiple mail register

--- EZsmart/src/main/java/com/test/security/auth/AuthenticationService.java
+++ EZsmart/src/main/java/com/test/security/auth/AuthenticationService.java
@@ -26,7 +26,10 @@ public class AuthenticationService {
     private final EmailService emailService;
 
     public AuthenticationResponse register(RegisterRequest request) {
-
+        //check if user exists
+        if(repository.findByEmail(request.getEmail()) != null) {
+            throw new RuntimeException("Email already registered");
+        }
         var user = User.builder()
                 .firstname(request.getFirstname())
                 .lastname(request.getLastname())

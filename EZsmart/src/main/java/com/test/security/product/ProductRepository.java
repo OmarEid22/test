7ac3f0e6fe22472c699ccc8b19ad5a08ed3f0bc5
myyyh03 <myyy003@hotmail.com>
2025-06-16 23:57:32 +0300

    Edited Discount Price in Filtering products API

--- EZsmart/src/main/java/com/test/security/product/ProductRepository.java
+++ EZsmart/src/main/java/com/test/security/product/ProductRepository.java
@@ -19,6 +19,6 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
     Product findByIdAndSellerId(Long productId, Long sellerId);
 
-    @Query("SELECT p FROM Product p WHERE (:categoryId IS NULL OR p.category.id = :categoryId) AND (:discountPrice IS NULL OR p.discountPrice = :discountPrice) AND (:specialOffer IS NULL OR p.specialOffer = :specialOffer) AND (:priceRangeMin IS NULL OR p.price >= :priceRangeMin) AND (:priceRangeMax IS NULL OR p.price <= :priceRangeMax)")
+    @Query("SELECT p FROM Product p WHERE (:categoryId IS NULL OR p.category.id = :categoryId) AND (:discountPrice IS NULL OR p.discountPrice >= :discountPrice) AND (:specialOffer IS NULL OR p.specialOffer = :specialOffer) AND (:priceRangeMin IS NULL OR p.price >= :priceRangeMin) AND (:priceRangeMax IS NULL OR p.price <= :priceRangeMax)")
     List<Product> searchProducts(@Param("categoryId") Long categoryId, @Param("discountPrice") Double discountPrice, @Param("specialOffer") Boolean specialOffer, @Param("priceRangeMin") Double priceRangeMin, @Param("priceRangeMax") Double priceRangeMax);
 }

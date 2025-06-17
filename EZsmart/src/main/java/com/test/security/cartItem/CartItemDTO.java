f448e1c22579259ae5216ad588e7ea409e507504
myyyh03 <myyy003@hotmail.com>
2025-06-17 03:03:43 +0300

    Edited CartItemDTO

--- EZsmart/src/main/java/com/test/security/cartItem/CartItemDTO.java
+++ EZsmart/src/main/java/com/test/security/cartItem/CartItemDTO.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import com.test.security.user.User;
+import com.test.security.product.Product;
 
 import java.time.LocalDateTime;
 
@@ -15,20 +16,14 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class CartItemDTO {
     private Long id;
-    private Long productId;
-    private String productName;
-    private String productImage;
-    private Double price;
+    private Product product;
     private Integer quantity;
     private LocalDateTime createdAt;
     private User user;
     
     public CartItemDTO(CartItem cartItem) {
         this.id = cartItem.getId();
-        this.productId = cartItem.getProduct().getId();
-        this.productName = cartItem.getProduct().getName();
-        this.productImage = cartItem.getProduct().getImage();
-        this.price = cartItem.getProduct().getPrice();
+        this.product = cartItem.getProduct();
         this.quantity = cartItem.getQuantity();
         this.createdAt = cartItem.getCreatedAt();
         this.user = cartItem.getUser();

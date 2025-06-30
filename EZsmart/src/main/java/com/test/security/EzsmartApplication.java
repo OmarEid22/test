c4c092321fc092173b68e2fc85fe9aaec5b11f00
myyyh03 <myyy003@hotmail.com>
2025-06-30 17:46:45 +0300

    Edited local Language

--- EZsmart/src/main/java/com/test/security/EzsmartApplication.java
+++ EZsmart/src/main/java/com/test/security/EzsmartApplication.java
@@ -3,11 +3,15 @@ package com.test.security;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Locale;
+
 @SpringBootApplication
 public class EzsmartApplication {
 
 	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+
 		SpringApplication.run(EzsmartApplication.class, args);
 	}
-
 }
